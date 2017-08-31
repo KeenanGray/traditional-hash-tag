a3db27be6c3f6fe72e58b19999ba687719078589
Keenan Gray <krgray@wpi.edu>
2017-08-31 17:04:29 -0400

    added upload button

--- index.js
+++ index.js
@@ -33,67 +33,43 @@ app.get('/JoinGame', function (request, response) {
     response.render('pages/GameRunning');
 });
 
-var uploading = false;
-
 //The upload code was found here.
 //https://coligo.io/building-ajax-file-uploader-with-node/
 //Modified by me to use Cloudinary
-app.post('/Upload', function (req, res) {
-    if (!uploading) {
-        uploading = true;
-        // create an incoming form object
-        var form = new formidable.IncomingForm();
+app.post('/Upload', function (req, response) {
+    console.log("post");
+    // create an incoming form object
+    var form = new formidable.IncomingForm();
 
-        // specify that we want to allow the user to upload multiple files in a single request
-        form.multiples = true;
+    // specify that we want to allow the user to upload multiple files in a single request
+    form.multiples = true;
 
-        // store all uploads in the /uploads directory
-        form.uploadDir = path.join(__dirname, '/uploads');
+    // store all uploads in the /uploads directory
+    form.uploadDir = path.join(__dirname, '/uploads');
 
-        // every time a file has been uploaded successfully,
-        // rename it to it's orignal name
-        form.on('file', function (field, file) {
-            fs.rename(file.path, path.join(form.uploadDir, file.name));
-        });
+    // every time a file has been uploaded successfully,
+    // rename it to it's orignal name
+    form.on('file', function (field, file) {
+        fs.rename(file.path, path.join(form.uploadDir, file.name));
 
-        form.on('file', function (field, file) {
-            cloudinary.uploader.upload(path.join(form.uploadDir, file.name),
-                function (result) {
-                    uploading = false//console.log(result) 
-                })
-        });
+        cloudinary.uploader.upload(path.join(form.uploadDir, file.name),
+            function (result) {
+                console.log("uploaded");
+                    })
+    });
 
-        // parse the incoming request containing the form data
-        if (req)
-            form.parse(req);
+    // log any errors that occur
+    form.on('error', function (err) {
+        console.log('An error has occured: \n' + err);
+    });
 
-    }
-    else {
-        console.log("a problem with the upload");
-    }
-    //// store all uploads in the /uploads directory
-    //form.uploadDir = path.join(__dirname, '/uploads');
+    // once all the files have been uploaded, send a response to the client
+    form.on('end', function () {
+        response.end('success');
+    });
 
-    //// every time a file has been uploaded successfully,
-    //// rename it to it's orignal name
-    //form.on('file', function (field, file) {
-    //    fs.rename(file.path, path.join(form.uploadDir, file.name));
-    //});
-
-    //// log any errors that occur
-    //form.on('error', function (err) {
-    //    console.log('An error has occured: \n' + err);
-    //});
-
-    //// once all the files have been uploaded, send a response to the client
-    //form.on('end', function () {
-    //    res.end('success');
-    //});
-
-    //// parse the incoming request containing the form data
-    //if (req)
-    //    form.parse(req);
-
+    // parse the incoming request containing the form data
+    form.parse(req);
 });
 
 
