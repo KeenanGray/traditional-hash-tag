31a1a645f6e3118d84d8278a1fbf5cf5727221fc
Keenan Gray <krgray@wpi.edu>
2017-08-31 14:35:46 -0400

    updated cloudinary

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ var app = express();
 var formidable = require('formidable');
 var path = require('path');
 var fs = require('fs');
+require('dotenv').config()
+
+
+cloudinary.config({
+    cloud_name: process.env.cloudinary_cloud_name,
+    api_key: process.env.cloudinary_api_key,
+    api_secret: process.env.cloudinary_api_secret
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -38,29 +46,32 @@ app.post('/Upload', function (req, res) {
     // specify that we want to allow the user to upload multiple files in a single request
     form.multiples = true;
 
-    // store all uploads in the /uploads directory
-    form.uploadDir = path.join(__dirname, '/uploads');
-
-    // every time a file has been uploaded successfully,
-    // rename it to it's orignal name
     form.on('file', function (field, file) {
-        fs.rename(file.path, path.join(form.uploadDir, file.name));
+        cloudinary.uploader.upload(file.name,
+            function (result) { console.log(result) })
     });
 
-    // log any errors that occur
-    form.on('error', function (err) {
-        console.log('An error has occured: \n' + err);
-    });
+    //// store all uploads in the /uploads directory
+    //form.uploadDir = path.join(__dirname, '/uploads');
 
-    // once all the files have been uploaded, send a response to the client
-    form.on('end', function () {
-        res.end('success');
-    });
+    //// every time a file has been uploaded successfully,
+    //// rename it to it's orignal name
+    //form.on('file', function (field, file) {
+    //    fs.rename(file.path, path.join(form.uploadDir, file.name));
+    //});
 
-    // parse the incoming request containing the form data
-    form.parse(req);
-    //cloudinary.uploader.upload(selectedFile,
-    //    function (result) { alert(result) })
+    //// log any errors that occur
+    //form.on('error', function (err) {
+    //    console.log('An error has occured: \n' + err);
+    //});
+
+    //// once all the files have been uploaded, send a response to the client
+    //form.on('end', function () {
+    //    res.end('success');
+    //});
+
+    //// parse the incoming request containing the form data
+    //form.parse(req);
 
 });
 
